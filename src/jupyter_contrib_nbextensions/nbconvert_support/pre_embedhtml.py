3aac78ed222711555b9062d0ae49a2df619a4304
Gabriel Nuetzi <gnuetzi@gmail.com>
2017-09-18 21:49:39 +0200

    added missing file

--- src/jupyter_contrib_nbextensions/nbconvert_support/pre_embedhtml.py
+++ src/jupyter_contrib_nbextensions/nbconvert_support/pre_embedhtml.py
@@ -2,7 +2,7 @@
 """Nbconvert preprocessor for the embedding img sources into the cells."""
 
 from nbconvert.preprocessors import Preprocessor
-from .embedhtml import EmbedHTMLExporter, HT, ImgTagTransform
+from .embedhtml import EmbedHTMLExporter, et
 
 
 class PyMarkdownPreprocessor(Preprocessor, EmbedHTMLExporter):
@@ -29,18 +29,22 @@ class PyMarkdownPreprocessor(Preprocessor, EmbedHTMLExporter):
             Index of the cell being processed (see base.py)
         """
 
+        self.path = resources['metadata']['path']
         if cell.cell_type == "markdown":
             if 'attachments' in cell.keys():
                 self.attachments += cell['attachments']
             # Parse HTML and replace <img> tags with the embedded data
-            transformer = HT()
-            transformer.pushTagTransform(ImgTagTransform(
-                path=resources['metadata']['path'],
-                log=self.log))
-            transformer.feed(cell)
+            parser = et.HTMLParser()
+            root = et.fromstring(cell, parser=parser)
+            nodes = root.findall(".//img")
+
+            for n in nodes:
+                # replfunc comes from the EmbedHTMLExporter class, and is all
+                # that is really needed from there
+                self.replfunc(n)
 
             # Convert back to HTML
-            embedded_output = transformer.get_html()
+            embedded_output = et.tostring(root, method="html")
 
             return embedded_output, resources
         else:
